8745ae7b4d76e75eb04947f6f573550e9e031e28
lanshuai <lanshuai@meituan.com>
2023-07-26 11:03:45 +0800

    feat: 引入样式

--- src/core/editor.ts
+++ src/core/editor.ts
@@ -1,34 +1,32 @@
-
- 
-
-import { IEditorConfig } from './types'
+import { IEditorConfig } from "./types";
 export class Editor {
-  private config: Partial<IEditorConfig>
+  private config: Partial<IEditorConfig>;
 
-  private container: Element
+  private container: Element;
 
   constructor(config: IEditorConfig) {
-    this.config = config
-    this.container = this.getContainer(config.selector)
-    console.log(this.container)
-    this.container.setAttribute('contenteditable', 'true')
+    this.config = config;
+    this.container = this.getContainer(config.selector);
+    this.container.classList.add("blue-editor-container");
+    console.log(this.container);
+    this.container.setAttribute("contenteditable", "true");
   }
 
   public getContainer(selector: string) {
     if (!selector) {
-      throw new Error('please input query selector of the container')
+      throw new Error("please input query selector of the container");
     }
 
-    const container = document.querySelectorAll(selector)
+    const container = document.querySelectorAll(selector);
 
     if (!container.length) {
-      throw new Error('query selector did not match element')
+      throw new Error("query selector did not match element");
     }
 
     if (container.length > 1) {
-      throw new Error('query selector matched multiple elements')
+      throw new Error("query selector matched multiple elements");
     }
 
-    return container[0]
+    return container[0];
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
-import CompositionAPI from '@vue/composition-api'
+import Vue from "vue";
+import App from "./App.vue";
+import CompositionAPI from "@vue/composition-api";
+import "@/core/style/style.scss";
 
-Vue.use(CompositionAPI)
+Vue.use(CompositionAPI);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
